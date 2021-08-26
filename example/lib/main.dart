bdb3c4ad263926028d421394e59f22a67faa9185
Zahniar Adirahman <zahniar88@gmail.com>
2021-08-27 00:45:37 +0700

    delete second screen

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:example/second_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_markdown_editor/simple_markdown_editor.dart';
 
@@ -68,6 +67,24 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key, required this.data}) : super(key: key);
+
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Markdown Parse"),
+      ),
+      body: ZMarkdownParse(
+        data: data,
       ),
     );
   }
